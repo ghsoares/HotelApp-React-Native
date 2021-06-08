d06a6157d7f1d39d858fcfd78de81a3506b53d52
GHSoares <ghsoares99795@gmail.com>
2021-06-07 21:11:55 -0300

    .

--- components/HotelsContainer.js
+++ components/HotelsContainer.js
@@ -3,9 +3,9 @@ import { FlatList, StyleSheet, Text, View } from 'react-native';
 import HotelItem from './HotelItem';
 
 export default class HotelsContainer extends React.Component {
-    sortData = (a,b) => {
+    /*sortData = (a,b) => {
         return a.price - b.price;
-    }
+    }*/
 
     renderItem = (item) => {
         const {name,price,address,listId} = item.item;
@@ -23,7 +23,7 @@ export default class HotelsContainer extends React.Component {
         return (
             <FlatList
                 style={styles.list}
-                data={this.props.data.sort(this.sortData)}
+                data={this.props.data}
                 renderItem={this.renderItem}
                 keyExtractor={item => item.id.toString()}
             />
